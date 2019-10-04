cc103b21957673806c73fa233ce044b830c3e76e
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-04 05:24:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -260,9 +260,9 @@ def long_name_steals_a_ton?
   
   
   game_hash.each do |location, team_data|
-    team_data[:players].each do |player, stats|
-      if stats[:steals] > steals
-        steals = stats[:steals]
+    team_data[:players].each do |player_hash|
+      if player_hash[:steals] > steals
+        steals = player_hash[:steals]
         player_with_most_steals = player
 end
 end
