f2be5013b0ee6bf948fd95e223413cdc5cf1fbed
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-10-04 05:17:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -255,7 +255,7 @@ def player_with_longest_name
 end        
 
 def long_name_steals_a_ton?
-  steals = []
+  all_steals = []
   most_steals = nil
   player_with_most_steals = nil
   
@@ -263,6 +263,12 @@ def long_name_steals_a_ton?
   game_hash.each do |location, team_data|
     team_data[:players].each do |player_hash|
     player_hash.each do |player_name, player_data|
-    
+           all_steals << player_data[:steals]
+        most_steals = all_steals.max
+        player_with_most_steals = player_name if player_data[:steals] == most_steals
+      end
+    end
+  end
 
-end 
+  return true if player_with_longest_name == player_with_most_steals
+end
